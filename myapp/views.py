8ca7514647a10f0860ca6c9705895bc9d085c682
omidiora <omidioraemmanuel@gmail.com>
2020-03-14 22:04:49 +0100

    fix bug

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,23 @@
 import requests
 from django.shortcuts import render
+from requests.compat import quote_plus
 from bs4 import BeautifulSoup
+from .models import Search
+from . import models
 
 # Create your views here.
+BASE_CRAIGSLIST_URL="https://johannesburg.craigslist.org/d/education-teaching/search/edu"
 def home(request):
     return render(request,'base.html')
 
 def new_search(request):
     search=request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url=BASE_CRAIGSLIST_URL.format(quote_plus(search)) 
+    response=requests.get(final_url)
+    data=response.text
+    print(data)
+
     stuff_for_frontend={
         'search':search
 
