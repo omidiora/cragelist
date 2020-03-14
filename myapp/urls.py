a334b2993a4c6d119f086c1a59069f8d5e914de6
omidiora <omidioraemmanuel@gmail.com>
2020-03-14 08:45:59 +0100

    templates new

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
       
       path('',views.home,name='home'),
-] 
+      path('newsearch/',views.new_search,name='new_search'),
+]  
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def new_search(request):
+    search=request.POST.get('search')
+    stuff_for_frontend={
+        'search':search
+
+    }
+    return render(request,'myapp/new_search.html',stuff_for_frontend)
